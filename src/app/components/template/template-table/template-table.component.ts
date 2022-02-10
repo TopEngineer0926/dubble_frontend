f2033f2981b81af07ca3e3a50172195c418cfc00
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-02-10 09:01:59 -0500

    feat: wrote the code to modify the template page for special domain for special customers

--- src/app/components/template/template-table/template-table.component.ts
+++ src/app/components/template/template-table/template-table.component.ts
@@ -184,7 +184,11 @@ export class TemplateTableComponent implements OnInit, OnDestroy {
           this.currentProduct = data?.products?.currentProduct;
         const product = this.currentProduct;
         // Duplicate the current product
-        this.productPageLink = `${environment.webUrl}${product.product.share_code}`;
+        var domain = environment.webUrl;
+        if (product.product.customer.domain_name)
+          domain = `https://${product.product.customer.domain_name}/`;
+
+        this.productPageLink = `${domain}${product.product.share_code}`;
         this.uploadedVideo = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Video);
         this.uploadedImage = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Image)[0];
         this.uploadedPdf = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Pdf);
@@ -229,7 +233,11 @@ export class TemplateTableComponent implements OnInit, OnDestroy {
         this.currentProduct = data?.products?.currentProduct;
         const product = this.currentProduct;
         // Duplicate the current product
-        this.productPageLink = `${environment.webUrl}${product.product.share_code}`;
+        var domain = environment.webUrl;
+        if (product.product.customer.domain_name)
+          domain = `https://${product.product.customer.domain_name}/`;
+
+        this.productPageLink = `${domain}${product.product.share_code}`;
         this.uploadedVideo = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Video);
         this.uploadedImage = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Image)[0];
         this.uploadedPdf = product.media?.list.filter(({ media_type }) => media_type && media_type === MediaType.Pdf);
